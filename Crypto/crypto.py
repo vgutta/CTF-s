d00a024f1811328a7eec1e21304713235ccb00a3
Vineeth Gutta <vineethgutta@gmail.com>
2018-10-05 23:31:34 +0000

    crypto

---
+++ Crypto/crypto.py
@@ -0,0 +1,88 @@
+key = '\x1a:\x0f\xd3'
+ket=y.encode('hex')
+ket.encode('hex')
+key.encode('hex')
+int(key.encode('hex'))
+int(key.encode('hex'), 16)
+ciphertext = '{Tk\xaa'
+int(key.encode('hex'), 16) ^ int(ciphertext.encode('hex'), 16)
+interval = int(key.encode('hex'), 16) ^ int(ciphertext.encode('hex'), 16)
+intval = int(key.encode('hex'), 16) ^ int(ciphertext.encode('hex'), 16)
+format('x', intval)
+format(intval, 'x')
+format(intval, 'x').decode('hex')
+from Crypto.Cipher import AES
+aes = AES.new(key='andy love simone', mode=AES.MODE_ECB)
+aes.encrypt('bananas')
+aes.encrypt('bananas'*2+'!!')
+aes.decrypt(_)
+'411cc6934095156422f8fcf7df681a884e71fdecef8e985364f9367cc542ccaa3faa9c502cb0f174c79e403093fd8992'.decode('hex')
+aes.decrypt('411cc6934095156422f8fcf7df681a884e71fdecef8e985364f9367cc542ccaa3faa9c502cb0f174c79e403093fd8992')
+aes.decrypt('A\x1c\xc6\x93@\x95\x15d"\xf8\xfc\xf7\xdfh\x1a\x88Nq\xfd\xec\xef\x8e\x98Sd\xf96|\xc5B\xcc\xaa?\xaa\x9cP,\xb0\xf1t\xc7\x9e@0\x93\xfd\x89\x92')
+aes.decrypt('39ffa7fbad7a60e3aa4aa4deeaa1c95b')
+'\xf8\xc5\x93\xc9)\xbc\xc2zvE\xa0\xfc"\x07\x8f\x8f\xd0\xe6ui\x9aa\xd2\x85\x1c\x07D\xe0z\xefB \x93C\xa8n-\x7f\xd2]\x17\x00 \x0c\x04\x88\x9d\x1ds\x8a\x80,\xcf\xd9A\xa8D\x15\x0e`S\xe7\x81?\xa4N^\x0c\xa5d6\xb7\n!~\xbd\x90\x81\xaa\xddu\x15\xf1\x97ku:\xce\xe0"J\x97\x8aRH\xa1'
+_.decode('hex')
+\xf8\xc5\x93\xc9)\xbc\xc2zvE\xa0\xfc"\x07\x8f\x8f\xd0\xe6ui\x9aa\xd2\x85\x1c\x07D\xe0z\xefB \x93C\xa8n-\x7f\xd2]\x17\x00 \x0c\x04\x88\x9d\x1ds\x8a\x80,\xcf\xd9A\xa8D\x15\x0e`S\xe7\x81?\xa4N^\x0c\xa5d6\xb7\n!~\xbd\x90\x81\xaa\xddu\x15\xf1\x97ku:\xce\xe0"J\x97\x8aRH\xa1'.decode('hex')
+aes = AES.new(key='andy love simone', mode=AES.MODE_ECB)
+aes.decrypt('A\x1c\xc6\x93@\x95\x15d"\xf8\xfc\xf7\xdfh\x1a\x88Nq\xfd\xec\xef\x8e\x98Sd\xf96|\xc5B\xcc\xaa?\xaa\x9cP,\xb0\xf1t\xc7\x9e@0\x93\xfd\x89\x92')
+'\x06\xd6\xe6\xe2\x8c\xcf\xb2\xb9g\xc9\xff]n`\x9b\x81f\xb2I\xc2\xa3\xef\x83\xdcB\xfc\xdd\x982\xe6\xfa\xe5\xb0Vq0\x97H"\xc1\xe7\xdf\xcfb\x14\xdd\xf6\x0c'.encode('hex)
+'\x06\xd6\xe6\xe2\x8c\xcf\xb2\xb9g\xc9\xff]n`\x9b\x81f\xb2I\xc2\xa3\xef\x83\xdcB\xfc\xdd\x982\xe6\xfa\xe5\xb0Vq0\x97H"\xc1\xe7\xdf\xcfb\x14\xdd\xf6\x0c'.encode('hex')
+'39ffa7fbad7a60e3aa4aa4deeaa1c95b'.decode("hex")
+aes.decrypt('9\xff\xa7\xfb\xadz`\xe3\xaaJ\xa4\xde\xea\xa1\xc9[')
+'411cc6934095156422f8fcf7df681a884e71fdecef8e985364f9367cc542ccaa3faa9c502cb0f174c79e403093fd8992'.decode('hex')
+aes.encrypt('A\x1c\xc6\x93@\x95\x15d"\xf8\xfc\xf7\xdfh\x1a\x88Nq\xfd\xec\xef\x8e\x98Sd\xf96|\xc5B\xcc\xaa?\xaa\x9cP,\xb0\xf1t\xc7\x9e@0\x93\xfd\x89\x92')
+aes.decrypt('\x96\n\xa2)\x91\x87{w7AX\xb3\x9eg_\xd8k\xa7&N\xbd\xc8W MD\xbe\xb7d\x128\x1a\x03\xe4\xb5r\xcf\x96 \xe2YR\x15^+\xdbJ\x05')
+'A\x1c\xc6\x93@\x95\x15d"\xf8\xfc\xf7\xdfh\x1a\x88Nq\xfd\xec\xef\x8e\x98Sd\xf96|\xc5B\xcc\xaa?\xaa\x9cP,\xb0\xf1t\xc7\x9e@0\x93\xfd\x89\x92'.encode('hex')
+'A\x1c\xc6\x93@\x95\x15d"\xf8\xfc\xf7\xdfh\x1a\x88Nq\xfd\xec\xef\x8e\x98Sd\xf96|\xc5B\xcc\xaa?\xaa\x9cP,\xb0\xf1t\xc7\x9e@0\x93\xfd\x89\x92'.decode('hex')
+'411cc6934095156422f8fcf7df681a884e71fdecef8e985364f9367cc542ccaa3faa9c502cb0f174c79e403093fd8992'.decode('hex')
+aes.decrypt('A\x1c\xc6\x93@\x95\x15d"\xf8\xfc\xf7\xdfh\x1a\x88Nq\xfd\xec\xef\x8e\x98Sd\xf96|\xc5B\xcc\xaa?\xaa\x9cP,\xb0\xf1t\xc7\x9e@0\x93\xfd\x89\x92')
+aes.decrypt('\x06\xd6\xe6\xe2\x8c\xcf\xb2\xb9g\xc9\xff]n`\x9b\x81f\xb2I\xc2\xa3\xef\x83\xdcB\xfc\xdd\x982\xe6\xfa\xe5\xb0Vq0\x97H"\xc1\xe7\xdf\xcfb\x14\xdd\xf6\x0c')
+!python --version
+key = 'andy loves simone'
+from Crypto.Ciphers import AES
+from Crypto.Cipher import AES
+AES.
+aes = AES new(key, AES.MODE_CBC, '000102030405060708090a0b0c0d0e0f'.decode('hex'))
+aes = AES.new(key, AES.MODE_CBC, '000102030405060708090a0b0c0d0e0f'.decode('hex'))
+aes = AES.new('andy loves simone', AES.MODE_CBC, '000102030405060708090a0b0c0d0e0f'.decode('hex'))
+aes = AES.new('andy love simone', AES.MODE_CBC, '000102030405060708090a0b0c0d0e0f'.decode('hex'))
+aes.decrypt("87dd2acb714db44393d8b4b71bdbad7720fbf40d2e34a03a93324cb9c4b97a08")
+aes.decrypt("87dd2acb714db44393d8b4b71bdbad7720fbf40d2e34a03a93324cb9c4b97a08".decode('hex'))
+aes.decrypt("87dd2acb714db44393d8b4b71bdbad7720fbf40d2e34a03a93324cb9c4b97a08".decode('hex'))
+aes.decrypt("87dd2acb714db44393d8b4b71bdbad7720fbf40d2e34a03a93324cb9c4b97a08".decode('hex'))
+aes = AES.new('andy love simone', AES.MODE_CBC, '000102030405060708090a0b0c0d0e0f'.decode('hex'))
+aes.decrypt("87dd2acb714db44393d8b4b71bdbad7720fbf40d2e34a03a93324cb9c4b97a08".decode('hex'))
+iv = '000102030405060708090a0b0c0d0e0f'.decode('hex')
+def xor(a, b):
+        assert len(a) == len(b)
+        return ''.join([chr(ord(ai)^ord(bi)) for ai, bi in zip(a,b)])
+1half = xor('abcdefghijklmnop', iv)
+1half = xor('abcdefghijklmnop', iv)
+half = xor('abcdefghijklmnop', iv)
+aes = AES.new('andy love simone', AES.MODE_CBC, '000102030405060708090a0b0c0d0e0f'.decode('hex'))
+aes.encrypt(half).encode('hex')
+aes = AES.new('andy love simone')#, AES.MODE_CBC, '000102030405060708090a0b0c0d0e0f'.decode('hex'))
+aes.encrypt(half).encode('hex')
+import Image
+from Crypto import Random
+from Crypto.Cipher import AES
+im = Image.open('heckert_gnu.png')
+im
+import io
+img = io.imread('heckert_gnu.png')
+import base64
+with open("t.png", "rb") as imageFile:
+    str = base64.b64encode(imageFile.read())
+    str = base64.b64encode(imageFile.read())
+with open('heckert_gnu.png', 'rb') as imageFile:
+    str = base64.b64encode(imageFile.read())
+    print str
+with open('heckert_gnu.png', 'rb') as imageFile:
+    b = bytearray(f)
+with open('heckert_gnu.png', 'rb') as imageFile:
+    f = imageFile.read()
+    b = bytearray(f)
+b[0]
+b
+im.size
+%history -f crypto.py
